ddde7dbc675ecc765ddf1eb769e87c5bff4fbd45
Adrian Baker <baker.adrian94@gmail.com>
2020-11-13 14:46:28 -0500

    Make socket server configurable

--- src/utils/api/socket.js
+++ src/utils/api/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-const ENDPOINT = 'http://127.0.0.1:4000';
+const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT || 'http://127.0.0.1:4000';
 const socket = socketIOClient(ENDPOINT);
 console.log('Connected socket!');
 
